ddbc6b28a44fde90724a4488ba5f22705f430d65
sofie-rk <sofierk@stud.ntnu.no>
2020-09-09 11:50:33 +0200

    fixed typo in an

--- fourier_series_n.py
+++ fourier_series_n.py
@@ -1,5 +1,5 @@
 '''
-Plots the function and its fourier series for different upper limit of n (Nmax)
+Plots the function and its fourier serie for a given upper limit of n (Nmax)
 
 f(x) ~ a0 + sum(n=1, Nmax)(an*cos(n*pi*x/L) + bn*sin(n*pi*x/L))
 
@@ -8,7 +8,8 @@ Need to:
     define an in an(n)
     define bn in bn(n)
     define a0 (scalar)
-    provide lower limit (x_lower), upper limit (x_upper), maximum n for the sum (Nmax)
+    Nmax_list contains the n-max-values we want to plot
+    provide lower limit (x_lower), upper limit (x_upper), length (L)
     string describing the function f(x) (function_string) for nice plotting
     number of evaluation points (points)
 
@@ -21,10 +22,11 @@ import matplotlib.pyplot as plt
 def fourier_series(x, a0, Nmax):
     value = a0
 
-    for n in range(1, Nmax):
+    for n in range(1, Nmax+1):
         value += an(n)*cos(n*pi*x/L) + bn(n)*sin(n*pi*x/L)
     
     return value
+
 
 
 ### EXAMPLE ###
@@ -33,7 +35,7 @@ def function(x):
     return x**2
 
 def an(n):
-    return (-1)**n * 2/n**2
+    return (-1)**n * 4/(pi*n)**2
 
 def bn(n):
     return 0
@@ -42,16 +44,22 @@ x_lower = -1
 x_upper = 1
 L = 1
 a0 = 1/3
-Nmax = 5
+Nmax_list = [i for i in range(1,5)]
 function_string = "$x^2$"
 points = 51
 
 x_list = linspace(x_lower, x_upper, points)
-fourier_series_list = fourier_series(x_list, a0, Nmax)
 function_list = function(x_list)
 
-
 plt.plot(x_list, function_list, label=function_string)
-plt.plot(x_list, fourier_series_list, label="Fourier series")
+
+for nmax in Nmax_list:
+
+    fourier_series_list = fourier_series(x_list, a0, nmax)
+    
+    plt.plot(x_list, fourier_series_list, label="Fourier series n =" + str(nmax))
+
 plt.legend()
 plt.show()
+
+
--- fourier_series_simple.py
+++ fourier_series_simple.py
@@ -1,5 +1,5 @@
 '''
-Plots the function and its fourier serie for a given upper limit of n (Nmax)
+Plots the function and its fourier series for different upper limit of n (Nmax)
 
 f(x) ~ a0 + sum(n=1, Nmax)(an*cos(n*pi*x/L) + bn*sin(n*pi*x/L))
 
@@ -8,8 +8,7 @@ Need to:
     define an in an(n)
     define bn in bn(n)
     define a0 (scalar)
-    Nmax_list contains the n-max-values we want to plot
-    provide lower limit (x_lower), upper limit (x_upper), length (L)
+    provide lower limit (x_lower), upper limit (x_upper), maximum n for the sum (Nmax)
     string describing the function f(x) (function_string) for nice plotting
     number of evaluation points (points)
 
@@ -20,14 +19,12 @@ import matplotlib.pyplot as plt
 
 
 def fourier_series(x, a0, Nmax):
-
     value = a0
 
-    for n in range(1, Nmax+1):
+    for n in range(1, Nmax):
         value += an(n)*cos(n*pi*x/L) + bn(n)*sin(n*pi*x/L)
     
     return value
-
 
 
 ### EXAMPLE ###
@@ -36,7 +33,7 @@ def function(x):
     return x**2
 
 def an(n):
-    return (-1)**n * 2/n**2
+    return (-1)**n * 4/(pi*n)**2
 
 def bn(n):
     return 0
@@ -45,23 +42,16 @@ x_lower = -1
 x_upper = 1
 L = 1
 a0 = 1/3
-Nmax_list = [i for i in range(1,5)]
+Nmax = 5
 function_string = "$x^2$"
 points = 51
 
 x_list = linspace(x_lower, x_upper, points)
+fourier_series_list = fourier_series(x_list, a0, Nmax)
 function_list = function(x_list)
 
+
 plt.plot(x_list, function_list, label=function_string)
-
-for nmax in Nmax_list:
-
-    fourier_series_list = fourier_series(x_list, a0, nmax)
-    print(fourier_series_list)
-    label = "Fourier series n =" + str(nmax)
-    plt.plot(x_list, fourier_series_list, label=label)
-
+plt.plot(x_list, fourier_series_list, label="Fourier series")
 plt.legend()
 plt.show()
-
-
